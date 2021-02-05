e80a8a992a80e11388fe0de4cb75efbcfd9ed14a
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-02-05 16:40:49 +0600

    total donation

--- donation_site/views.py
+++ donation_site/views.py
@@ -5,36 +5,43 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.db.models import Sum
 import requests
 from donation_site.forms import CreateUserForm, CreatePostForm, CreateDonationForm
-from donation_site.models import Post
+from donation_site.models import Post, Donation
 
 def index(request):
     posts = Post.objects.all().order_by('-id')
+    total_donation = Donation.objects.aggregate(Sum('amount'))
     context = {
         'title': 'Home | Donation Center',
         'post_list': posts,
+        'total_donation': total_donation,
     }
     template = loader.get_template('post_list.html')
     return HttpResponse(template.render(context, request))
 
 def about(request):
-    context = {'title': 'About | Donation Center'}
+    total_donation = Donation.objects.aggregate(Sum('amount'))
+    context = {'title': 'About | Donation Center', 'total_donation': total_donation,}
     template = loader.get_template('about.html')
     return HttpResponse(template.render(context, request))
 
 @login_required(login_url='login_page')
 def my_posts(request):
     posts = Post.objects.filter(author=request.user.id).order_by('-id')
+    total_donation = Donation.objects.aggregate(Sum('amount'))
     context = {
         'title': 'Posts | Donation Center',
         'post_list': posts,
+        'total_donation': total_donation,
     }
     template = loader.get_template('post_list.html')
     return HttpResponse(template.render(context, request))
 
 def organization(request, pk):
     posts = Post.objects.filter(author=pk).order_by('-id')
+    total_donation = Donation.objects.aggregate(Sum('amount'))
     author = pk
     donate = True
     context = {
@@ -42,23 +49,27 @@ def organization(request, pk):
         'post_list': posts,
         'donate': donate,
         'author': author,
+        'total_donation': total_donation,
     }
     template = loader.get_template('post_list.html')
     return HttpResponse(template.render(context, request))
 
 def post(request, pk):
     post = Post.objects.get(id=pk)
+    total_donation = Donation.objects.aggregate(Sum('amount'))
     donate = False
     context = {
         'title': 'Post | Donation Center',
         'post': post,
         'donate': donate,
+        'total_donation': total_donation,
     }
     template = loader.get_template('post_detail.html')
     return HttpResponse(template.render(context, request))
 
 @login_required(login_url='login_page')
 def create_post(request):
+    total_donation = Donation.objects.aggregate(Sum('amount'))
     if request.method == 'POST':
         form = CreatePostForm(request.POST, request.FILES)
         if form.is_valid:
@@ -70,11 +81,13 @@ def create_post(request):
     context = {
         'title': 'New Post | Donation Center',
         'form': form,
+        'total_donation': total_donation,
     }
     template = loader.get_template('post_form.html')
     return HttpResponse(template.render(context, request))
 
 def registration_page(request):
+    total_donation = Donation.objects.aggregate(Sum('amount'))
     if request.user.is_authenticated:
         return redirect('index')
     else:
@@ -91,11 +104,13 @@ def registration_page(request):
     context = {
         'title': 'Registration | Donation Center',
         'form': form,
+        'total_donation': total_donation,
     }
     template = loader.get_template('registration.html')
     return HttpResponse(template.render(context, request))
 
 def login_page(request):
+    total_donation = Donation.objects.aggregate(Sum('amount'))
     if request.user.is_authenticated:
         return redirect('index')
     else:
@@ -113,6 +128,7 @@ def login_page(request):
 
         context = {
             'title': 'Login | Donation Center',
+            'total_donation': total_donation,
         }
         template = loader.get_template('login.html')
         return HttpResponse(template.render(context, request))
@@ -123,6 +139,7 @@ def logout_page(request):
 
 def donation_page(request, pk):
     author = User.objects.get(id=pk)
+    total_donation = Donation.objects.aggregate(Sum('amount'))
     form = CreateDonationForm()
     # if request.method == 'POST':
     #     form = CreatePostForm(request.POST, request.FILES)
@@ -135,13 +152,14 @@ def donation_page(request, pk):
         if form.is_valid:
             print('validated')
             form.save()
-        #     return redirect('index')
+            return redirect('index')
         else:
             print('validation failed')
     context = {
         'title': 'Donate | Donation Center',
         'author': author,
         'form': form,
+        'total_donation': total_donation,
     }
     template = loader.get_template('donate.html')
     return HttpResponse(template.render(context, request))
